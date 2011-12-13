1f0bf0af0c8aa3f111b5c1e0af8e0d730f82db9a
nickik <nickik@gmx.ch>
2011-12-14 00:11:07 +0100

    first commit

---
+++ init.el
@@ -0,0 +1,106 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the Emacs Starter Kit
+;;
+;; This is the first thing to get loaded.
+;;
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
+;; Turn off mouse interface early in startup to avoid momentary display
+;; You really don't need these; trust me.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Load path etc.
+
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+;; Load up ELPA, the package manager
+
+(add-to-list 'load-path dotfiles-dir)
+
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+(require 'package)
+(package-initialize)
+(require 'starter-kit-elpa)
+
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+;; backport some functionality to Emacs 22 if needed
+(require 'dominating-file)
+
+;; Load up starter kit customizations
+
+(require 'starter-kit-defuns)
+(require 'starter-kit-bindings)
+(require 'starter-kit-misc)
+(require 'starter-kit-registers)
+(require 'starter-kit-eshell)
+(require 'starter-kit-lisp)
+(require 'starter-kit-perl)
+(require 'starter-kit-ruby)
+
+(regen-autoloads)
+(load custom-file 'noerror)
+
+;; You can keep system- or user-specific customizations here
+(setq system-specific-config (concat dotfiles-dir system-name ".el")
+      user-specific-config (concat dotfiles-dir user-login-name ".el")
+      user-specific-dir (concat dotfiles-dir user-login-name))
+(add-to-list 'load-path user-specific-dir)
+
+(if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+(if (file-exists-p user-specific-dir)
+  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
+
+(add-to-list 'load-path "/home/nick/elisp/auto-complete")
+(add-to-list 'load-path "/home/nick/.emacs.d/ac-slime/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "/home/nick/elisp/auto-complete/ac-dict")
+(ac-config-default)
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+
+(eval-after-load 'clojure-mode
+  '(define-clojure-indent
+     (describe 'defun)
+     (testing 'defun)
+     (given 'defun)
+     (using 'defun)
+     (with 'defun)
+     (it 'defun)
+     (do-it 'defun)))
+
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+
+(setq load-path (cons "~/.emacs.d/dylan-mode" load-path))
+(autoload 'dylan-mode "dylan-mode" "Dylan-mode" t)
+(setq auto-mode-alist (cons '("\\.dylan\\'" . dylan-mode) auto-mode-alist))
+
+(add-to-list 'load-path "~/.emacs.d/slime")  ; your SLIME directory
+(setq inferior-lisp-program "/opt/opendylan-2011.1/bin/dswank") ; your dswank binary
+(require 'slime)
+(slime-setup '(slime-dylan slime-repl))
+
+;;; init.el ends here
